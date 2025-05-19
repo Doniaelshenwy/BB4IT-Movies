31d6f71f5e7da5e026d764e047088ccecb58def6
Donia Elshenawy <doniaelshenawyali@gmail.com>
2025-05-20 00:47:55 +0300

    Change core file paths

--- BB4IT-Movies/App/AppDelegate.swift
+++ BB4IT-Movies/App/AppDelegate.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import AlamofireEasyLogger
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let alamofireLogger = FancyAppAlamofireLogger(prettyPrint: true) {
+        print($0)
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
