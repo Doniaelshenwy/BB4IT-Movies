0abc0b5f22fbdda6f3ad48865ba989af7635c642
Donia Elshenawy <doniaelshenawyali@gmail.com>
2025-05-20 00:46:51 +0300

    Add app constants values

---
+++ BB4IT-Movies/Manager/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  BB4IT-Movies
+//
+//  Created by Donia Elshenawy on 19/05/2025.
+//
+
+import Foundation
+
+final class Constants {
+    static var baseURL = "https://api.themoviedb.org/3/movie/"
+    
+    static var bearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxM2ZiYThkOWQyMGY5YjJjMTRjZGU3YTNjMTMxZDZlMCIsIm5iZiI6MTY2MDEyMzUwNS4xOTYsInN1YiI6IjYyZjM3OTcxNTk0Yzk0MDA3ZjQ3ZDAyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a3K_F9DguAlaIGzH2XKiNX38Vzg4_6K_Gc19ZF7CrnY"
+}
